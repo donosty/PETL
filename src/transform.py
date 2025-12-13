aecacb0a7feebb4dc76abb1c60485e26a2fb66d1
donosty <mzac0077@gmail.com>
2025-12-13 00:20:00 -0600

    update transform.py

--- src/transform.py
+++ src/transform.py
@@ -1,7 +1,14 @@
 import pandas as pd
+import numpy as np
+
+from pandas.api.types import is_float_dtype, is_numeric_dtype, is_datetime64_any_dtype
 from extract import ruta_archivos_sucios
 
 lista_df = []
+
+df_fechas = pd.DataFrame({
+    "fecha":["2025/01/01", "2025/01/02", "2025/01/03", "NaN"]
+})
 
 for i in ruta_archivos_sucios:
     
@@ -9,29 +16,66 @@ for i in ruta_archivos_sucios:
     lista_df.append(df)
 
 class DataTransformer:
+    
     def __init__(self):
         pass
+    
     
     def _normalizar_encabezados(self, df_list):
         
         encabezados_traducidos = ['id_venta', 'fecha', 'articulo', 'cantidad', 'precio', 'ubicacion']
-        
-        extraccion_df_columnas = df_list[0].columns
-        encabezados_viejos = [e for e in extraccion_df_columnas]
-        
-        mapeo = dict(zip(encabezados_viejos, encabezados_traducidos))
-    
         dfs_renombrados = []
         
         for df in df_list:
-            
-            df.rename(columns=mapeo)    
+            df.columns = encabezados_traducidos  
             dfs_renombrados.append(df)
             
-        print(dfs_renombrados)
         return dfs_renombrados
     
     
+    def _unificar_dataframe(self, df_list):
+        
+        df_concatenado = pd.concat(df_list, axis=0, ignore_index=True)
+        return df_concatenado
+    
+    
+    def _limpiar_fechas_y_manejar_nulos(self, df):
+        
+        df['fecha'] = pd.to_datetime(df["fecha"], format='mixed', errors='coerce')
+            
+        df_limpio = df.dropna(subset=['fecha'])
+        return df_limpio
+    
+    
+    def _limpiar_texto(str, df):
+        
+        df['articulo'] = df['articulo'].str.upper()
+        df['ubicacion'] = df['ubicacion'].str.capitalize()
+        
+        return df
+    
+    
+    def _manejar_nulos_y_tipos_numericos(self, df):
+        
+        cantidad_int = is_numeric_dtype(df['cantidad'])
+        precio_float = is_float_dtype(df['precio'])
+        
+        if not cantidad_int or not precio_float:
+            df['cantidad'] = df['cantidad'].astype(np.int64)
+            df['precio'] = df['precio'].astype(np.float64)
+            
+        promedio_precios = df['precio'].mean()
+        df = df.fillna({'precio':promedio_precios})
+        
+        return df
+    
     
 obj = DataTransformer()
-obj._normalizar_encabezados(lista_df)
+dfs1 = obj._normalizar_encabezados(lista_df)
+dfs2 = obj._unificar_dataframe(dfs1)
+#print(dfs2.info())
+dfs3 = obj._limpiar_fechas_y_manejar_nulos(df_fechas)
+print(dfs2)
+
+#dfs4 = obj._limpiar_texto(dfs2)
+#print(dfs4)
